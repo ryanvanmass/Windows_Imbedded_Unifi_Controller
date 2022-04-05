5a1b922dc54da74e697363a90ad5b34e6225c146
Ryan Van Massenhoven <65045018+ryanvanmass@users.noreply.github.com>
2022-04-04 21:42:25 -0400

    V1 Feature Freeze

---
+++ Raw Files/Setup.ps1
@@ -0,0 +1,19 @@
+﻿# Install Java
+Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\$env:username\Downloads\Java.exe
+C:\Users\$env:username\Downloads\Java.exe
+Start-Sleep 120
+
+# Install Unifi Controller
+Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\$env:username\Downloads\UniFi-installer.exe
+C:\Users\$env:username\Downloads\UniFi-installer.exe
+Start-Sleep 120
+
+#Add Unifi Service
+java -jar 'C:\Users\$env:username\Ubiquiti UniFilib\ace.jar' installsvc
+
+# Gets Current IP for Port Forward
+$IP = (Get-WmiObject -class win32_NetworkAdapterConfiguration -Filter 'ipenabled = "true"').ipaddress[0]
+[Environment]::SetEnvironmentVariable("IPAddress", $ip, "User")
+
+#Forward Port 8443 to 443
+netsh interface portproxy add v4tov4 listenaddress=$IP listenport=443 connectaddress=$IP connectport=8443
