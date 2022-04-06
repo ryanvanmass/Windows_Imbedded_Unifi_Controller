b7e0093859349d8e14569a26ccfdd802ea743b11
Ryan <ryanvanmassenhoven@gmail.com>
2022-04-05 22:30:36 -0400

    Implement Potential Fix for Compiled executable not recognizing the Username Enviroment variable

--- Raw Files/Setup.ps1
+++ Raw Files/Setup.ps1
@@ -1,15 +1,18 @@
-﻿# Install Java
-Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\$env:username\Downloads\Java.exe
-C:\Users\$env:username\Downloads\Java.exe
+﻿#Create Username Variable
+$USERNAME = whoami
+
+# Install Java
+Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\$USERNAME\Downloads\Java.exe
+C:\Users\$USERNAME\Downloads\Java.exe
 Start-Sleep 120
 
 # Install Unifi Controller
-Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\$env:username\Downloads\UniFi-installer.exe
-C:\Users\$env:username\Downloads\UniFi-installer.exe
+Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\$USERNAME\Downloads\UniFi-installer.exe
+C:\Users\$USERNAME\Downloads\UniFi-installer.exe
 Start-Sleep 120
 
 #Add Unifi Service
-java -jar 'C:\Users\$env:username\Ubiquiti UniFilib\ace.jar' installsvc
+java -jar 'C:\Users\$USERNAME\Ubiquiti UniFilib\ace.jar' installsvc
 
 # Gets Current IP for Port Forward
 $IP = (Get-WmiObject -class win32_NetworkAdapterConfiguration -Filter 'ipenabled = "true"').ipaddress[0]
