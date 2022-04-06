736d65de69d94756756b69451ad44de51bc80502
Ryan <ryanvanmassenhoven@gmail.com>
2022-04-06 08:55:04 -0400

    Removed Username Variable due to reliability issues

--- Raw Files/Setup.ps1
+++ Raw Files/Setup.ps1
@@ -1,19 +1,15 @@
-﻿#Create Username Variable
-$USERNAME =( ( Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty username ) -split '\\' )[1]
-
-
-# Install Java
-Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\$USERNAME\Downloads\Java.exe
-C:\Users\$USERNAME\Downloads\Java.exe
+﻿# Install Java
+Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\Ryan\Downloads\Java.exe 
+C:\Users\Ryan\Downloads\Java.exe
 Start-Sleep 120
 
 # Install Unifi Controller
-Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\$USERNAME\Downloads\UniFi-installer.exe
-C:\Users\$USERNAME\Downloads\UniFi-installer.exe
+Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\Ryan\Downloads\UniFi-installer.exe
+C:\Users\Ryan\Downloads\UniFi-installer.exe
 Start-Sleep 120
 
 #Add Unifi Service
-java -jar 'C:\Users\$USERNAME\Ubiquiti UniFilib\ace.jar' installsvc
+java -jar 'C:\Users\Ryan\Ubiquiti UniFilib\ace.jar' installsvc
 
 # Gets Current IP for Port Forward
 $IP = (Get-WmiObject -class win32_NetworkAdapterConfiguration -Filter 'ipenabled = "true"').ipaddress[0]
