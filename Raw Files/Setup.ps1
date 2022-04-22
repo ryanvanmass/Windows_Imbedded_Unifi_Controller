397b91f4e94834ae06b1a1d59a610aab52a4aab9
Ryan <ryanvanmassenhoven@gmail.com>
2022-04-21 22:10:54 -0400

    Began Implementing Service Account

--- Raw Files/Setup.ps1
+++ Raw Files/Setup.ps1
@@ -9,20 +9,29 @@ $UserSelection = Read-Host -Prompt "Selection: "
 
 ### Install Controller ###
 if ($UserSelection -eq 1) {
+    # Create "Service" User Account
+    Write-Output 'Please Enter a password for the Controller "Service" Account'
+    $Password = Read-Host -AsSecureString
+
+    New-localUser -Name Controller -Password $Password -PasswordNeverExpires
+    Add-LocalGroupMember Users Controller
+
+    $Credentials = Get-Credential -UserName Controller -Message "Please Enter Controller Service Account Password"
+
     # Install Java
     Write-Output "Downloading Java"
-    Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\Ryan\Downloads\Java.exe 
+    Invoke-WebRequest https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245807_df5ad55fdd604472a86a45a217032c7d -OutFile C:\Users\Controller\Downloads\Java.exe 
 
     Write-Output "Installing Java"
-    C:\Users\Ryan\Downloads\Java.exe /s
+    Start-Process C:\Users\Controller\Downloads\Java.exe /s -Credential $Credentials
     Start-Sleep 120
 
     # Install Unifi Controller
     Write-Output "Downloading Unifi Controller"
-    Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\Ryan\Downloads\UniFi-installer.exe
+    Invoke-WebRequest https://dl.ui.com/unifi/7.0.25/UniFi-installer.exe -OutFile C:\Users\Controller\Downloads\UniFi-installer.exe
 
     Write-Output "Please Install Unifi Controller"
-    C:\Users\Ryan\Downloads\UniFi-installer.exe
+    Start-Proccess C:\Users\Controller\Downloads\UniFi-installer.exe -Credential $Credentials
     Pause
 
     # Get Current IP Address
@@ -39,13 +48,13 @@ if ($UserSelection -eq 1) {
     Exit
 }
 elseif ($UserSelection -eq 2) {
-    Set-Location 'C:\Users\Ryan\Ubiquiti UniFi'
+    Set-Location 'C:\Users\Controller\Ubiquiti UniFi'
     # Kill Unifi if it is running
     Stop-Process -Name Java*
     
     # Add Unifi Service
     Write-Output "Installing Controller as a Service"
-    java -jar 'C:\Users\Ryan\Ubiquiti UniFi\lib\ace.jar' installsvc
+    java -jar 'C:\Users\Controller\Ubiquiti UniFi\lib\ace.jar' installsvc
  
     # Start Service
     Start-Service Unifi
@@ -54,6 +63,6 @@ elseif ($UserSelection -eq 2) {
     Exit
 }
 elseif ($UserSelection -eq 3) {
-    Get-Content -Tail 30 "C:\Users\Ryan\Ubiquiti UniFi\logs\server.log" -Wait
+    Get-Content -Tail 30 "C:\Users\Controller\Ubiquiti UniFi\logs\server.log" -Wait
 }
 
